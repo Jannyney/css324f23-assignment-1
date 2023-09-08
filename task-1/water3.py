d1a3dff7739650b3c0b4cb8f166013f3490c3f4d
Janny Orlando <57592173+Jannyney@users.noreply.github.com>
2023-09-08 20:07:05 +0700

    add condition in qn 1

--- task-1/water3.py
+++ task-1/water3.py
@@ -1,9 +1,27 @@
 def initial_state():
-    return (0, 0, 0)
+    return (8, 0, 0)
 
 def is_goal(s):
-    return True
+    return s[0] == 4 and s[1] == 4
 
 def successors(s):
     x, y, z = s
-    return []
+    t = 5 - y
+    if x>0 and t>0:
+        if x>t:
+            yield((x-t,5,z), t)
+        else:
+            yield((0,y+x,z), x)
+    t = 8 - x
+    if y>0 and t>0:
+        if y>t:
+            yield((8,y-t,z), t)
+        else:
+            yield((x+y,0,z), y)
+    t = 3 - z
+    if x>0 and t>0:
+        if x>t:
+            yield((x-t,y,3), t)
+        else:
+            yield((0,y,z+x), x)
+    
