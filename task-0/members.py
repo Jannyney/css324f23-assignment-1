fb5a5ba6b407e24273a12162e7430597c6cf4f15
Janny Orlando <57592173+Jannyney@users.noreply.github.com>
2023-09-08 13:47:55 +0700

    added name

--- task-0/members.py
+++ task-0/members.py
@@ -1,4 +1,4 @@
 [
-    {"name": "Cholwich Nattee", "sid": "6422771111"},
-    {"name": "Cholwich Nattee", "sid": "6422771111"},
+    {"name": "Tawanrat Piriyalertsak", "sid": "6422781581"},
+    {"name": "Chaiwat Bunsrang", "sid": "6422782795"},
 ]
