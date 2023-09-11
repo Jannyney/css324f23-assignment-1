e75eeec322816ad5ba7095b7a061c1263cc24fa3
ShowKunz <101887065+ShowKunz@users.noreply.github.com>
2023-09-11 18:00:18 +0700

    Update eight_puzzle.py

--- task-2/eight_puzzle.py
+++ task-2/eight_puzzle.py
@@ -1,6 +1,7 @@
 import copy
 import math as m
 
+
 def initial_state():
     return ((7, 2, 4, 5, 0, 6, 8, 3, 1), 1, 1)
 
@@ -11,16 +12,16 @@ def is_goal(s):
 
 def successors(s):
     _, r, c = s
-    new_r, new_c = r-1, c
+    new_r, new_c = r - 1, c
     if is_valid(new_r, new_c):
         yield move_blank(s, new_r, new_c), 1
-    new_r, new_c = r+1, c
+    new_r, new_c = r + 1, c
     if is_valid(new_r, new_c):
         yield move_blank(s, new_r, new_c), 1
-    new_r, new_c = r, c-1
+    new_r, new_c = r, c - 1
     if is_valid(new_r, new_c):
         yield move_blank(s, new_r, new_c), 1
-    new_r, new_c = r, c+1
+    new_r, new_c = r, c + 1
     if is_valid(new_r, new_c):
         yield move_blank(s, new_r, new_c), 1
 
@@ -32,9 +33,10 @@ def is_valid(r, c):
 def move_blank(s, new_r, new_c):
     board, r, c = s
     new_board = list(board)
-    new_board[r*3 + c] = new_board[new_r*3 + new_c]
-    new_board[new_r*3 + new_c] = 0
+    new_board[r * 3 + c] = new_board[new_r * 3 + new_c]
+    new_board[new_r * 3 + new_c] = 0
     return (tuple(new_board), new_r, new_c)
+
 
 def h1(s):
     goal = (1, 2, 3, 4, 5, 6, 7, 8, 0)
@@ -48,29 +50,26 @@ def h1(s):
             res += 1
     return res
 
+def h2(s):
+    # calculate to sum of Manhattan distances
+    board, _, _ = s
+    res = 0
+    for idx, tile in enumerate(board):
+        if tile > 0:
+            goal_location = tile-1
+            current_location = idx
+            gr, gc = goal_location//3, goal_location%3
+            cr, cc = current_location//3, current_location%3
+            res += abs(gr-cr) + abs(gc-cc)
+    return res
+
 def h3(s):
-    #implement this function
-    # goal = ([1, 2, 3], 
-    #         [4, 5, 6], 
-    #         [7, 8, 0])
     res = 0
     board, _, _ = s
-    for idx in range(0,9):
-        if board[idx] == 0: 
-            if (m.ceil((idx + 1) / 3) != 3):    #Check if the index is either 6, 7, or 8 (Row 3)
+    for idx in range(0, 9):
+        if board[idx] != 0:
+            if m.ceil(board[idx] / 3) != m.ceil((idx + 1) / 3):  # Check row
                 res += 1
-            if (((idx + 1) % 3) != 0):  #Check if the index is either 3, 6, or 9 (Col 3)
+            if (board[idx] % 3) != ((idx + 1) % 3):  # Check col
                 res += 1
-        else:
-            if (m.ceil(board[idx] / 3) != m.ceil((idx + 1) / 3)): #Check row
-                res += 1
-            if ((board[idx] % 3) != ((idx + 1) % 3)): #Check col
-                res += 1
-    #วิธีกลุ่มกานต์ + นัท (เอามาเทียบ performance ดู)
-    # goal = (1,2,3,4,5,6,7,8,0)  
-    # for idx in range(1,9):
-    #     if (goalrow := (goal[idx] - 1) // 3) != (boardrow := (board[idx]-1) // 3):
-    #         res += abs(goalrow - boardrow)
-    #     if (goalcol := goal[idx] % 3) != (boardcol := board[idx] % 3):
-    #         res += abs(goalcol - boardcol)
     return res
